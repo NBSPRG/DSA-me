8cd5677ea397a551191789f13daddf0ab5d9ea85
chandan kumar <116447157+NBSPRG@users.noreply.github.com>
2023-09-28 14:13:11 +0530

    Create sorting array by parity.cpp

---
+++ sorting array by parity.cpp
@@ -0,0 +1,27 @@
+// 905. Sort Array By Parity
+
+// we will check first element whether it's odd or even;
+// if it's even then we will just move ahead by icrementing our 'i' pointer;
+// else we will replace first element with the last and then j-- means one odd element has been positioned correctly so, now for next we do j--;
+// we will do it till i < j 
+
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+    //    sorting array br parity
+    // using two pointer approach
+        int i = 0, j = nums.size()-1;
+        while(i < j)
+        {
+            if(nums[i] & 1)
+            {
+                swap(nums[i], nums[j]);
+                j--;
+            }
+
+            else i++;
+        }
+        return nums;
+    }
+};
